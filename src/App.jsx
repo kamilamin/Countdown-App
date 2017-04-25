4670c8ee9dab43fd23b6dcd622c3f3045e2c8381
kamil amin <Kamil.amin@live.co.uk>
2017-04-25 11:57:15 +0500

    This is the countdown app using react

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import Clock from './Clock';
+import {Form, FormControl, Button} from 'react-bootstrap';
+
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            deadline: 'December, 25 2017',
+            newDeadline: ''
+        }
+    }
+    changeDeadline(){
+ //       console.log('state', this.state)
+        this.setState({deadline: this.state.newDeadline});
+    }
+    
+    render(){
+        return(
+            <div className='App'>
+                <div>Countdown to {this.state.deadline}</div>
+                    <Clock 
+                        deadline={this.state.deadline} />
+                <Form inline>
+                    <FormControl type="text" placeholder='Enter new Date' className='Deadline-input'
+                    onChange={event => this.setState({newDeadline: event.target.value})} />
+
+                    <Button onClick={() => this.changeDeadline()}>Submit</Button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default App;
